14f84758e94987fd1ce91e76fd905c28e19c26f0
manoti <manotimike@gmail.com>
2023-04-12 12:25:42 +0300

    books data

---
+++ src/books.js
@@ -0,0 +1,32 @@
+export const books = [
+  {
+    id: 1,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71aG+xDKSYL._AC_UL127_SR127,127_.jpg',
+    title: 'The 48 Laws of Power',
+    author: 'Robert Greene',
+  },
+  {
+    id: 2,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71aG+xDKSYL._AC_UL127_SR127,127_.jpg',
+    title: 'The 48 Laws of Power',
+    author: 'Robert Greene',
+    img: 'https://m.media-amazon.com/images/I/31+T+6iSEdL._SX330_BO1,204,203,200_.jpg',
+    title: 'Girl in Pieces',
+    author: 'Kathleen Glasgow',
+  },
+  {
+    id: 3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71aG+xDKSYL._AC_UL127_SR127,127_.jpg',
+    title: 'The 48 Laws of Power',
+    author: 'Robert Greene',
+    img: 'https://m.media-amazon.com/images/I/41T5H8u7fUL._SX331_BO1,204,203,200_.jpg',
+    title: "You Don't Know JS Yet",
+    author: 'Kyle Simpson ',
+  },
+  {
+    id: 4,
+    img: 'https://m.media-amazon.com/images/I/51InjRPaF7L._SX377_BO1,204,203,200_.jpg',
+    title: 'Eloquent JavaScript, 3rd Edition',
+    author: ' Marijn Haverbeke ',
+  },
+]
